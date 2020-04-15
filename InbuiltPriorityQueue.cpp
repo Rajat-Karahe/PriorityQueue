7dd8b9b322d7ee795edabcc9185ca87c673399b8
Rajat-Karahe <29karaherajat@gmail.com>
2020-04-15 12:01:50 +0530

    Create InbuiltPriorityQueue.cpp

---
+++ InbuiltPriorityQueue.cpp
@@ -0,0 +1,33 @@
+//Inbuilt priority queue is implemented in max heap form
+
+#include<iostream>
+using namespace std;
+#include<queue>
+
+int main(){
+    priority_queue<int> pq;
+    
+    pq.push(16);
+    pq.push(1);
+    pq.push(167);
+    pq.push(7);
+    pq.push(45);
+    pq.push(32);
+    
+    cout << "Size: " << pq.size() << endl;
+    cout << "Top: " << pq.top() << endl; //Max value
+    
+    while(!pq.empty()){
+        cout << pq.top() << endl;
+        pq.pop();
+    }
+}
+
+//Size: 6
+//Top: 167
+//167
+//45
+//32
+//16
+//7
+//1
