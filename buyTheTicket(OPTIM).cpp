6fe430728f5992a2856173ef347a05a8c8f09bb3
Rajat-Karahe <29karaherajat@gmail.com>
2020-04-15 20:49:30 +0530

    Update buyTheTicket(OPTIM).cpp

--- buyTheTicket(OPTIM).cpp
+++ buyTheTicket(OPTIM).cpp
@@ -2,11 +2,6 @@
 
 #include<queue>
 int buyTicket (int *arr, int n, int k){
-    /* Don't write main().
-     * Don't read input, it is passed as function argument.
-     * Return output and don't print it.
-     * Taking input and printing output is handled automatically.
-     */
     priority_queue<int> pq;
     queue<int> q;
     
